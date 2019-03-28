808ce12f52e59d7641b359c73c194e57e4cfe48f
Jordi Raya Lapuente <raya.jordi@gmail.com>
2019-03-29 00:14:28 +0100

    Section 2.19: Angular Material

--- mean-course/src/app/app.module.ts
+++ mean-course/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatInputModule, MatCardModule, MatButtonModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/posts-create/post-create.component';
@@ -13,7 +15,11 @@ import { PostCreateComponent } from './posts/posts-create/post-create.component'
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
