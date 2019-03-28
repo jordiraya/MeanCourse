453d9ebb4a95c6e2aabca7b67c4d678954671d23
Jordi Raya Lapuente <raya.jordi@gmail.com>
2019-03-28 18:04:15 +0100

    Section 2: The Angular Frontend

---
+++ mean-course/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { PostCreateComponent } from './posts/posts-create/post-create.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostCreateComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
