2525d998e4336ae20a8412b7526be1fe0dd07071
Jordi Raya Lapuente <raya.jordi@gmail.com>
2019-04-01 16:52:12 +0200

    Section 2.22: structural directives

---
+++ mean-course/src/app/posts/posts-list/post-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent {
+  posts = [
+    {title: 'First Post', content: 'this is the first post\'s content'},
+    {title: 'Second Post', content: 'this is the second post\'s content'},
+    {title: 'Third Post', content: 'this is the third post\'s content'}
+  ];
+}
